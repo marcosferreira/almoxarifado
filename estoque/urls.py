6784254e83f5c62eb8ce7b7f5d17280c376cb826
Marcos Ferreira <marcosferreira.developer@gmail.com>
2026-04-15 16:48:46 -0300

    fix(urls): adicionar prefixo 'accounts/' nas rotas de login e logout

--- estoque/urls.py
+++ estoque/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     # Auth
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     
     # Dashboard
     path("", views.dashboard, name="dashboard"),
